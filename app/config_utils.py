054597e750ca694522c66e26385530107fd19476
Alessandro <scarcella.alex@gmail.com>
2026-03-28 09:01:51 +0100

    changed version 0.4.6 beta to 0.4.7 beta

--- app/config_utils.py
+++ app/config_utils.py
@@ -10,7 +10,7 @@ import sys
 from pathlib import Path
 from app.logger import logger
 
-__version__ = "0.4.6 beta"
+__version__ = "0.4.7 beta"
 
 # valori di default che poi vengono sovrascritti
 # dal file di configurazione JSON
